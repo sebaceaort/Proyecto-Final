5497074444d6f3645386d4f88ab10df61c8c62b0
Sebastian Cea <sceaort@gmail.com>
2021-10-26 18:40:47 -0300

    show children and children item

--- src/services/fiwareApi.js
+++ src/services/fiwareApi.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_MONGO_URL
+const api = process.env.REACT_APP_MONGO_URL_DEV
 
 async function getEntities() {
   let arr = [];
@@ -23,5 +23,49 @@ async function getEntities() {
   return arr;
 }
 
+async function getDataByType(type) {
+  let arr = [];
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Accept", "application/json");
+  myHeaders.append("Authorization",window.localStorage.getItem("token"))
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  await fetch(`${api}/fiware/entities/type/${type}`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      arr = res;
+    })
+    .catch((err) => console.log(err));
+
+  return arr;
+}
+async function getDataByQuery(query) {
+  let arr = [];
+  var myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+  myHeaders.append("Accept", "application/json");
+  myHeaders.append("Authorization",window.localStorage.getItem("token"))
+
+  var requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  await fetch(`${api}/fiware/entities/query/${query}`, requestOptions)
+    .then((response) => response.json())
+    .then((res) => {
+      arr = res;
+    })
+    .catch((err) => console.log(err));
+
+  return arr;
+}
 //eslint-disable-next-line
-export default { getEntities };
+export default { getEntities, getDataByQuery, getDataByType};
--- src/views/PruebasScreen.js
+++ src/views/PruebasScreen.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import { ShowArr } from '../components/ShowArr'
-import { data } from '../DataAux/data'
+import React, { useEffect, useState } from "react";
+import { ShowArr } from "../components/ShowArr";
+import fiwareApi from "../services/fiwareApi";
 
 export const PruebasScreen = () => {
-    const datos = data
-    
-    return (
-        <>
-        <h1>seccion de prueba</h1>
-        <div className=" d-flex flex-column conteiner-fluid bg-dark ">
-            <ShowArr array={datos}/>
-        </div>
-        </>
-    )
-}
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function getEntities() {
+      const entities = await fiwareApi.getDataByType("Municipio");
+      setData(entities);
+    }
+    getEntities();
+  }, []);
+
+  return (
+    <>
+      <h1>seccion de prueba</h1>
+      <div className=" d-flex flex-column conteiner-fluid bg-dark w-100 ">
+        <ShowArr array={data} />
+      </div>
+    </>
+  );
+};
