42c7590c0f025ea7dec4c34790005ddf58e49985
unknown <federicobecker97@hotmail.com>
2021-11-07 17:26:10 -0300

    Validacion cambiar meta

--- src/components/MunicipioButtons/UpdateGoal.js
+++ src/components/MunicipioButtons/UpdateGoal.js
@@ -1,36 +1,35 @@
-import React, { useContext, useState} from "react";
-import { Form, Button, Image, Modal, Spinner } from "react-bootstrap";
+import React, { useContext, useState } from "react";
+import { Form, Button, Image, Modal, Alert } from "react-bootstrap";
 import { UpdateContext } from "../../context/update-context";
 import firewareApi from "../../services/fiwareApi"
-
+import { Formik } from "formik";
 //agregar POST
 
 const AddGoalButton = ({ item }) => {
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const [formularioEnviado, cambiarFormularioEnviado] = useState(false);
+    // const [exito, cambiarExito] = useState(false);
     function AddGoalModal() {
         const [animate, setAnimate] = useState(false);
 
-        const [datos, setDatos] = useState({
-        });
+        const { setUpdate } = useContext(UpdateContext);
 
-        const handleChangeDatos = (value, prop) => {
-            setDatos({ ...datos, [prop]: value });
-        };
-
-        const {setUpdate} = useContext(UpdateContext);
-        async function handleSubmit() {
-
+        async function handleSubmit(valores) {
             const data = await createGoal(item.id)
             async function createGoal(id) {
-                return await firewareApi.postNewGoal(datos, id)
+                return await firewareApi.postNewGoal(valores, id)
             }
 
             if (data) {
-                alert("Meta de " + JSON.stringify(item.name) + " fue agregada exitosamente")
-                handleClose();
-                setUpdate(state=>!state)
+                // cambiarExito(true)
+                // if(cambiarExito){
+                //         <Alert>Meta actualizada con éxito!</Alert>
+                // }
+                // setTimeout(() => cambiarExito(false), 5000)            
+                setTimeout(() => handleClose(), 1000)
+                setUpdate(state => !state)
             } else {
                 setAnimate(false);
                 alert("Error");
@@ -44,58 +43,73 @@ const AddGoalButton = ({ item }) => {
                     fluid
                     className="centered-image"
                 />
-                <Form
-                    onSubmit={(e) => {
-                        e.preventDefault();
-                        setAnimate(true);
-                        handleSubmit();
+
+                <Formik
+                    initialValues={{
+                        monto: '',
+                        fecha: ''
+                    }}
+                    validate={(valores) => {
+                        let errores = {};
+                        let dias = Math.round((Date.now() - Date.parse(valores.fecha)) / (1000 * 60 * 60 * 24));
+
+                        if (!valores.monto) {
+                            errores.monto = ('Por favor ingrese un monto')
+                        } else if (isNaN(valores.monto)) {
+                            errores.monto = ('Por favor, ingrese un monto numérico')
+                        }
+                        if (!valores.fecha) {
+                            errores.fecha = ('Por favor ingrese una fecha')
+                        } else if (dias > 0) {
+                            errores.fecha = ('Por favor ingrese una fecha posterior a la actual')
+                        }
+                        return errores;
+                    }}
+                    onSubmit={(valores, { resetForm }) => {
+                        resetForm();
+                        console.log("Formulario enviado")
+                        cambiarFormularioEnviado(true)
+                        setTimeout(() => cambiarFormularioEnviado(false), 1000)
+                        handleSubmit(valores)
                     }}
                 >
-                    <Form.Group className="mb-3" controlId="formMetaMonto">
-                        <Form.Label>Monto esperado:</Form.Label>
-                        <Form.Control
-                            required
-                            type="Integer"
-                            placeholder="Monto"                                //ver VALIDATIONS!!!!!!!!!!!!
-                            onChange={(monto) => {
-                                handleChangeDatos(monto.target.value, "monto");
-                            }}
-                        />
-                    </Form.Group>
-                    <Form.Group className="mb-3" controlId="formMetaFecha">
-                        <Form.Label>Fecha limite:</Form.Label>
-                        <Form.Control
-                            required
-                            type="date"
-                            placeholder="Fecha"                                //ver VALIDATIONS!!!!!!!!!!!!
-                            onChange={(goalFecha) => {
-                                handleChangeDatos(goalFecha.target.value, "goalFecha");
-                            }}
-                        />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3">
-                        <Button
-                            variant="primary"
-                            type="submit"
-                            size="lg"
-                            className={"full-width"}
-                        >
-                            {animate ? (
-                                <Spinner
-                                    as="span"
-                                    animation="grow"
-                                    size="sm"
-                                    role="status"
-                                    aria-hidden="true"
+                    {({ values, errors, handleSubmit, handleChange, handleBlur, touched }) => (
+                        <form className="row" onSubmit={handleSubmit}>
+                            <div className="row mb-3">
+                                <label htmlFor="monto">Monto esperado</label>
+                                <input className="mb-3" controlId="formMetaFecha"
+                                    type="Integer"
+                                    id="monto"
+                                    name="monto"
+                                    placeholder="Ingrese el monto esperado"
+                                    value={values.monto}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
                                 />
-                            ) : (
-                                ""
-                            )}
-                            {!animate ? "Agregar meta" : "Loading..."}
-                        </Button>
-                    </Form.Group>
-                </Form>
+                                {touched.monto && errors.monto && <div style={{ color: 'red' }}>{errors.monto}</div>}
+                            </div>
+                            <div className="row mb-3">
+                                <label htmlFor="fecha">Fecha</label>
+                                <input
+                                    type="date"
+                                    id="fecha"
+                                    name="fecha"
+                                    placeholder="Ingrese una fecha"
+                                    value={values.fecha}
+                                    onChange={handleChange}
+                                    onBlur={handleBlur}
+                                />
+                                {touched.fecha && errors.fecha && <div style={{ color: 'red' }}>{errors.fecha}</div>}
+                            </div>
+                            <div>
+                                <button className="btn btn-primary" type="submit">Crear meta</button>
+                                {formularioEnviado && <p style={{ color: 'green' }}>Meta cargada con éxito!</p>}
+                            </div>
+                        </form>
+
+                    )}
+                </Formik>
+
             </div>
         );
     }
