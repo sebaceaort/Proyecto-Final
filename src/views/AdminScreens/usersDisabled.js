cb2501a90ac87e24e96b4765cebad8069f72e167
Leonardo Adamini <leonardoadamini@gmail.com>
2021-11-09 17:18:21 -0300

    Fix redirect after disable user

--- src/views/AdminScreens/usersDisabled.js
+++ src/views/AdminScreens/usersDisabled.js
@@ -58,7 +58,7 @@ export default function UsersDisabled() {
                       onClick={() => {
                         authApi.updateUsers(user._id);
                         alert("Usuario Deshabilitado!");
-                        window.location.reload();
+                        history.push("/UsersDisabled")
                       }}
                     >
                       Deshabilitar
